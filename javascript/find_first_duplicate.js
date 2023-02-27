83b9fc089ffc87bd4d389f485892aa3fb6560059
Chris Erlendson <ckesurf@gmail.com>
2023-02-27 11:05:46 -0500

    solution after demo

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,53 @@
+// @param: arr, an array
+// @return: integer
 function findFirstDuplicate(arr) {
-  // type your code here
+  /* 
+    Rewrite the problem in my own words:
+    Look for the 1st number that gets repeated. Otherwise, -1
+
+    Test cases:
+    [1, 2]        => -1
+    [2, 2]        => 2
+    [2, 1, 2]     => 2
+    [1, 2, 1, 2]  => 1
+
+    Pseudocode:
+    Keep track of numbers I've already seen
+    // array, object {key: value}
+    Iterate through each element
+    // for 
+      if an element was already seen, then return that number
+      // comparator, return, if
+      // Ruby: .uniq
+      otherwise, add that number to list of numbers I've already seen
+      // .push()
+
+    If there are no "hits", then return -1
+  */
+  // Keep track of numbers I've already seen
+  // array, object {key: value}
+  // const numbersThatWeveAlreadyIteratedThrough = [] //duplicates
+  const numbersAlreadySeen = []; //duplicates
+  // const seenAlready = [] //duplicates
+  // const numbersSeen = [] //duplicates
+
+  // Iterate through each element
+  for (let i = 0; i < arr.length; i++) {
+    //   if an element was already seen, then return that number
+    // comparator, return, if
+    // Ruby: .uniq
+    // element I am looking => arr[i]
+    if (numbersAlreadySeen.includes(arr[i])) {
+      return arr[i];
+    } else {
+      //   otherwise, add that number to list of numbers I've already seen
+      // .push()
+      numbersAlreadySeen.push(arr[i]);
+    }
+  }
+
+  // If there are no "hits", then return -1
+  return -1;
 }
 
 if (require.main === module) {
